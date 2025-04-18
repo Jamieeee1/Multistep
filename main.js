d1600cc32a6866fd8b33162e168a1a3c235c9153
Barnabas James <barnabasjames27@gmail.com>
2025-04-18 14:41:00 +0100

    correction?

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const nextBtn = document.querySelector('#nextBtn');
 const prevBtn = document.querySelector('#prevBtn');
 const navNums = document.querySelectorAll('.navButton')
+const steps = document.querySelectorAll('.step')
 
-const steps = document.querySelectorAll('.steps')
 let currentPage = 1;
 
 
@@ -10,7 +10,12 @@ const movefocus = () => {
     navNums.forEach((e)=> {
         e.classList.remove("active");
     })
+    steps.forEach((e)=> {
+        e.classList.remove("active");
+    })
+    
     navNums[currentPage-1].classList.add("active");
+    steps[currentPage-1].classList.add("active");
 }
 
 nextBtn.addEventListener('click', (e) => {
