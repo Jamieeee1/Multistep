4c38845065a13e6a6246b41a9ebda88a8ceabd3b
Barnabas James <barnabasjames27@gmail.com>
2025-04-24 22:05:16 +0100

    Completed the sum logic

--- main.js
+++ main.js
@@ -6,20 +6,28 @@ const toggle = document.querySelector('#switchtoggle');
 const monthly = document.querySelectorAll('.monthly');
 const yearly = document.querySelectorAll('.yearly');
 const plans = document.querySelectorAll('.plans');
-const checkbox = document.querySelectorAll('.Addons');
+const checkboxes = document.querySelectorAll('.Addons');
 
 //plan result
 const planName = document.querySelector('.p-name');
 const planPrice = document.querySelector('.p-price');
 
+//addons result 
+const addOnsResult = document.querySelector('.p-add-ons');
+
+//Total result
+const totalDiv = document.querySelector('.total-div');
 
 
 
 let phase = 'bymonth';
 let currentPage = 0;
 
-let sum;
+let sum1;
+let sum2;
+let sum3;
 let planPriceLabel;
+let addOnLabel;
 
 let currentPlan;
 let currentPlanPrice
@@ -52,9 +60,6 @@ const yearlyItems = {
 };
 
 // console.log(yearlyItems.monthlyAddons["Customizable profile"])
-console.log(Object.keys(yearlyItems));
-
-
 
 
 // functions
@@ -75,15 +80,31 @@ const priceLabel = (phase) => {
         return 'mo'
     } else {
         return 'yr'
-    }
+    };
 };
+
+const planLabel = (phase) => {
+    if(phase === 'bymonth') {
+        return 'month'
+    } else {
+        return 'year'
+    };
+}
 
 const getPlan = (planPriceLabel) => {
     if(phase==='bymonth') {
         return monthlyItems.monthyPlans[planPriceLabel]
     } else {
         return yearlyItems.yearlyPlans[planPriceLabel]
-    }
+    };
+};
+
+const getAddon = (addOnLabel) => {
+    if (phase === 'bymonth') {
+        return monthlyItems.monthlyAddons[addOnLabel]
+    } else {
+        return yearlyItems.yearlyAddons[addOnLabel]
+    };
 };
 
 const asignPlan = () => {
@@ -91,19 +112,41 @@ const asignPlan = () => {
         if (e.checked) {
             planPrice.textContent = '$' + getPlan(e.value) + '/' + priceLabel(phase);
             if(planPriceLabel === e.value) {
+                sum1 = getPlan(e.value)
                 return
             } else{
                 planPriceLabel = e.value
-                planName.textContent = e.value
-                console.log(e.value)
+                planName.textContent = `${e.value} (${planLabel(phase)}ly)`
+                sum1 = getPlan(e.value)
             };
         };
     });
+    console.log(`sum of plan = ${sum1}`)
 };
 
+const asignAddOns = () => {
+    addOnsResult.innerHTML = '';
+    sum2 = 0
+    for (let checkbox of checkboxes) {
+        if (checkbox.checked) {
+            addOnLabel = checkbox.value;
+            addOnsResult.innerHTML += `
+            <div>
+            <span>${checkbox.value}</span> <span class="addPrices">+$${getAddon(addOnLabel)}/${priceLabel(phase)}</span>
+            </div>
+            `
+            sum2 += getAddon(addOnLabel);
+        };
+    };
+    console.log(`sum of addons = ${sum2}`)
+};
 
-const add = () => {
-    
+const totalSum = (sum3) => {
+    sum3 = sum1 + sum2;
+
+    totalDiv.innerHTML = `
+        <span class="total-label">Total (per ${planLabel(phase)})</span> <span class="total-cost">$${sum3}/${priceLabel(phase)}</span>
+    `
 }
 
 
@@ -112,6 +155,10 @@ nextBtn.addEventListener('click', (e) => {
     e.preventDefault();
     if(currentPage == 1) {
         asignPlan()
+    }
+    if(currentPage ==2) {
+        asignAddOns()
+        totalSum()
     }
 
     if(currentPage>=3){
